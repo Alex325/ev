5fa08c7658d9db27fed0efed44509a8b3d5302e7
Alex <alexsandromacedopinto@gmail.com>
2026-06-20 03:25:37 -0300

    suporte a mover o cursor do teclado e quebra de linha.

--- src/document.c
+++ src/document.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <document.h>
 
 line_t line_new() {
@@ -31,11 +33,11 @@ void line_grow(line_t *this) {
 }
 
 void line_shift(line_t *this, int amount, int column) {
-    for (int i = this->size - 1; i >= column; i++)
+    for (int i = this->size - 1; i >= column; i--)
     {
         this->text[i + amount] = this->text[i];
     }
-    this->size++;
+    this->size += amount;
 }
 
 void line_add_char(line_t *this, unsigned char chr, int column) {
@@ -44,8 +46,36 @@ void line_add_char(line_t *this, unsigned char chr, int column) {
     this->text[column] = chr;
 }
 
+void line_add_text(line_t *this, char *txt, int size) {
+    if (this->size + size >= this->capacity) line_grow(this);
+    memcpy(this->text + this->size, txt, size);
+    this->size += size;
+}
+
 void document_add_char(document_t *this, unsigned char chr, int line, int column) {
     line_add_char(&this->lines[line], chr, column);
+}
+
+void document_grow(document_t *this) {
+    
+    this->capacity *= 2;
+    this->lines = realloc(this->lines, this->capacity * sizeof(*this->lines));
+}
+
+void document_shift(document_t *this, int line) {
+    for (int i = this->size - 1; i > line; i--)
+    {
+        this->lines[i + 1] = this->lines[i];
+    }
+    this->size++;    
+}
+
+void document_break_line(document_t *this, int line, int column) {
+    if (this->size == this->capacity) document_grow(this);
+    document_shift(this, line);
+    this->lines[line+1] = line_new();
+    line_add_text(&this->lines[line+1], this->lines[line].text + column, this->lines[line].size - column),
+    this->lines[line].size = column;
 }
 
 void line_destroy(const line_t *line) {
